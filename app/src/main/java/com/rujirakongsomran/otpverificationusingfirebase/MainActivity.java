9935bb606da3caef465722b0c43598ab5cf89161
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-10-15 15:33:44 +0700

    Enable ViewBinding and update version gradle

--- app/src/main/java/com/rujirakongsomran/otpverificationusingfirebase/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/otpverificationusingfirebase/MainActivity.java
@@ -3,12 +3,21 @@ package com.rujirakongsomran.otpverificationusingfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.rujirakongsomran.otpverificationusingfirebase.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
