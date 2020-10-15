85a4f6788954f56ce13d579e6d48c4102732ec64
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-10-15 17:17:29 +0700

    Part 1 : Setup Layout finish

--- app/src/main/java/com/rujirakongsomran/otpverificationusingfirebase/SendOTPActivity.java
+++ app/src/main/java/com/rujirakongsomran/otpverificationusingfirebase/SendOTPActivity.java
@@ -2,13 +2,42 @@ package com.rujirakongsomran.otpverificationusingfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.rujirakongsomran.otpverificationusingfirebase.databinding.ActivitySendOtpBinding;
 
 public class SendOTPActivity extends AppCompatActivity {
+
+    ActivitySendOtpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_send_otp);
+        //setContentView(R.layout.activity_send_otp);
+
+        binding = ActivitySendOtpBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        final EditText flEtInputMobile = binding.etInputMobile;
+
+        binding.btnGetOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (flEtInputMobile.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(SendOTPActivity.this, "Enter Mobile", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Intent intent = new Intent(getApplicationContext(), VerifyOTPActivity.class);
+                intent.putExtra("mobile", flEtInputMobile.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/rujirakongsomran/otpverificationusingfirebase/VerifyOTPActivity.java
+++ app/src/main/java/com/rujirakongsomran/otpverificationusingfirebase/VerifyOTPActivity.java
@@ -3,12 +3,127 @@ package com.rujirakongsomran.otpverificationusingfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+
+import com.rujirakongsomran.otpverificationusingfirebase.databinding.ActivityVerifyOtpBinding;
 
 public class VerifyOTPActivity extends AppCompatActivity {
+
+    ActivityVerifyOtpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_verify_otp);
+        //setContentView(R.layout.activity_verify_otp);
+
+        binding = ActivityVerifyOtpBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.tvMobile.setText(String.format(
+                "+66-%s", getIntent().getStringExtra("mobile")
+        ));
+
+        setupOTPInputs();
+
+    }
+
+    private void setupOTPInputs() {
+        binding.etInputCode1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    binding.etInputCode2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.etInputCode2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    binding.etInputCode3.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.etInputCode3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    binding.etInputCode4.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.etInputCode4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    binding.etInputCode5.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.etInputCode5.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    binding.etInputCode6.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
